7fa7cde03cb540ee1afc54abe00068ecc5a66715
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2020-12-19 12:57:36 +0900

    細かい修正不要な部分の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :kana_first_name, :kana_last_name, :birthday, ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birthday, ])
   end
 end
