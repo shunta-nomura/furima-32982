43730f8f2c8602930155c170b1005baa896e7730
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2020-12-20 19:49:27 +0900

    モデル単テストコード実装、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_burden_id
+      validates :shipping_day_id
+    end
+    validates :shipping_area_id, numericality: { other_than: 0 }
+    validates :price, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
+    validates :name
+    validates :description
+    validates :image
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
@@ -9,12 +23,4 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_day
 
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :status_id
-    validates :shipping_burden_id
-    validates :shipping_day_id
-  end
-  validates :shipping_area_id, numericality: { other_than: 0 }
-  validates :price, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,7 +9,79 @@ RSpec.describe Item, type: :model do
     it 'itemの全てのカラムが存在していれば保存ができる' do
       expect(@item).to be_valid
     end
+  end
 
-    
+  context '異常系' do
+    it '商品画像を1枚つけないと登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it '商品名が無いと登録できないこと' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it '商品の説明が無いと登録できないこと' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'カテゴリー情報が無いと登録できないこと' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it '商品の状態が無いと登録できないこと' do
+      @item.status_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+
+    it '配送料の負担が無いと登録できないこと' do
+      @item.shipping_burden_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping burden can't be blank")
+    end
+
+    it '配送料の地域が無いと登録できないこと' do
+      @item.shipping_area_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+    end
+
+    it '発送までの日時が無いと登録できないこと' do
+      @item.shipping_day_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+    end
+
+    it '価格の情報が無いと登録できないこと' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it '価格の範囲が、300円以下なら登録できないこと' do
+      @item.price = 100
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it '価格の範囲が、9999999以上なら登録できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it '価格は半角数字でないと登録できないこと' do
+      @item.price = '２２２'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
   end
 end
