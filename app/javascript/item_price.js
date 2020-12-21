9cfc0b57b5145cf224b343688849b811a584f932
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2020-12-21 11:01:30 +0900

    価格計算部分修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,11 +3,11 @@ function item_price() {
   priceInput.addEventListener("keyup", () => {
     const fee_minus = priceInput.value * 0.1
     const fee_minus_form = document.getElementById("add-tax-price")
-    fee_minus_form.innerHTML = fee_minus
+    fee_minus_form.innerHTML =  parseInt(fee_minus)
     
     const fee = priceInput.value * 0.9
     const fee_form = document.getElementById("profit")
-    fee_form.innerHTML = fee
+    fee_form.innerHTML = parseInt(fee)
   });
 }
 
