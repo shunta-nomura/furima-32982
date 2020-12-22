5c4fa660af2d729473500d9bf95de61ab99d4799
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2020-12-22 17:41:50 +0900

    beforeactionで処理まとめ
    current_user.id?の?でエラーが起きていたので削除
    コメント削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -18,28 +19,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user_id != current_user.id?
-      render :show
-    end
+    render :show if @item.user_id != current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit
     end
   end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_burden_id, :shipping_area_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
